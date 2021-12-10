fa6e4a0e8dd2237e503953651c56e9efe34d11cd
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-12-10 18:59:25 +0300

    rename

---
+++ src/components/editor/icons/v-icon-end-node.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" class="v-svg-icon">
+    <circle cx="7" cy="7" r="6.5" fill="none" stroke="currentColor" />
+    <path
+      d="M8.35938 7.04248H5.58643V9.31006H8.80762V10H4.74268V3.60156H8.76367V4.2959H5.58643V6.35254H8.35938V7.04248Z"
+    />
+  </svg>
+</template>
